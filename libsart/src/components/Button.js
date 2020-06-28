5c2be15cdfdc3cf7a4a629c7374d6f8c1ec21eb1
larafonse <nlarafonseca@csumb.edu>
2020-06-27 19:33:26 -0700

    details page completed

--- libsart/src/components/Button.js
+++ libsart/src/components/Button.js
@@ -5,15 +5,21 @@ export const ButtonContainer = styled.button`
     font-size:1.4rem;
     background:transparent;
     border: .05rem solid var(--lightBlue);
-    color:var(--lightBlue);
+    border-color: ${props=> 
+        props.cart? "var(--mainYellow)":"var(--lightBlue)"
+    };
+    color: ${prop=> 
+        prop.cart? "var(--mainYellow)": "var(--lightBlue)"
+    };
     border-radius:.7rem;
     padding:.2rem .5rem;
     cursor:pointer;
     margin:.2rem .5rem .2rem 0;
     transition: all .5s ease-in-out;
 &:hover{
+    background: ${prop=>
+        prop.cart? "var(--mainYellow)":"var(--lightBlue)"};
     color:var(--mainBlue);
-    border: .05rem solid var(--mainBlue);
 }
 &:focus{
     outline:none;
--- libsart/src/context.js
+++ libsart/src/context.js
@@ -6,14 +6,69 @@ const ProductContext = React.createContext();
 export default class ProductProvider extends Component {
     state = {
         products:storeProducts,
-        detailProduct:detailProduct
+        detailProduct:detailProduct,
+        cart:[],
+        modalOpen: true,
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     }
-    handleDetail = ()=>{
-        console.log('hello from detail');
+    componentDidMount(){
+        this.setProducts();
     }
-    addToCart = ()=>{
-        console.log('hello from add to cart');
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item=> {
+          const singleItem = {...item};
+          tempProducts = [...tempProducts, singleItem];
+        })
+        this.setState(()=> {
+          return {products: tempProducts};
+        })
     }
+    getItem = (id) => {
+        const product = this.state.products.find(item=>  item.id == id);
+        return product;
+    }
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(
+          ()=>{
+            return {detailProduct: product};
+          }
+        )
+      }
+    addToCart = (id) => {
+     let tempProducts = [...this.state.products];
+     const product = this.getItem(id);
+     const index = tempProducts.indexOf(product);
+     product.inCart = true;
+     product.count = 1;
+     const price = product.price;
+     product.total = price;
+
+     this.setState(()=>{
+       return {products:tempProducts, cart:[...this.state.cart, product]}
+     },
+     ()=>{
+    //    this.addTotals();
+        console.log('hello');
+     })
+  }
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return {modalProduct: product, modalOpen:true};
+    })
+  }
+
+  closeModal = () => {
+      this.setState(()=>{
+          return {modalOpen: false};
+      })
+  }
     render() {
         return (
             <ProductContext.Provider value= {{
